573d3bf54cb4b6347c3b4a636d0bea60782aaa1b
David S.S. Mendez <saurionx2005@gmail.com>
2021-03-31 13:27:44 -0300

    agrego la primera version del pinchi tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,9 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
+import Layout from '../components/Layout'
+ 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-         TEST started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <Layout>
+      <div className="container mx-auto p-4 text-center">The content from Storyblok will follow soon...</div>
+    </Layout>
   )
 }
