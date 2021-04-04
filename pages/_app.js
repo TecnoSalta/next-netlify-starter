f2a366354e411e568fe6e014701fabbc6939a4d9
David S.S. Mendez <saurionx2005@gmail.com>
2021-04-03 23:24:45 -0300

    inicio tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/tailwind.css'
+import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div className="mx-auto w-9/12">
+    <nav>
+      <ul>
+        <li>
+          <Link href="/"><a>Home</a></Link>
+          
+        </li>
+        <Link href="/about"><a>Acerca de</a></Link>
+      </ul>
+    </nav>
+    <Component {...pageProps} />
+    
+  </div>
+  )
+  
 }
 
 export default MyApp
